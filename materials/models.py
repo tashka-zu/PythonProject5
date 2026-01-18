e403b926d257f28c54c37eaa6db0ab022b3bb638
«tashka-zu» <zubkovanatska21@gmail.com>
2026-01-18 14:20:28 +0300

    добавлены приложения

---
+++ materials/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    preview = models.ImageField(upload_to='courses/', verbose_name='Превью', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='lessons/', verbose_name='Превью', blank=True, null=True)
+    video_link = models.URLField(verbose_name='Ссылка на видео', blank=True, null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='Курс')
+
+    def __str__(self):
+        return self.name
+
---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = models.CharField(max_length=35, verbose_name='Телефон', blank=True)
+    city = models.CharField(max_length=100, verbose_name='Город', blank=True)
+    avatar = models.ImageField(upload_to='avatars/', verbose_name='Аватар', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
