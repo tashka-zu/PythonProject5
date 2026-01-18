064f7f2175ea2967c7d99d28611027af0fa39ce7
«tashka-zu» <zubkovanatska21@gmail.com>
2026-01-18 14:48:11 +0300

    выполнены задачи и проверены в postman

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .managers import UserManager
 
 class User(AbstractUser):
     username = None
@@ -10,6 +11,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
