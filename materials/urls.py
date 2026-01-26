c1f3c90c3841ed2cbf7de6505a337fd4878ab8aa
«tashka-zu» <zubkovanatska21@gmail.com>
2026-01-26 14:17:44 +0300

    выполнены все задачи

--- materials/urls.py
+++ materials/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView
+from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView, LessonViewSet
 
 router = DefaultRouter()
+router.register(r'lessons', LessonViewSet, basename='lessons')
 router.register(r'courses', CourseViewSet)
 
 urlpatterns = [
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PaymentListView
+from .views import PaymentListView, UserRegistrationView
 
 urlpatterns = [
     path('payments/', PaymentListView.as_view(), name='payment-list'),
+    path('register/', UserRegistrationView.as_view(), name='user-register'),
 ]
